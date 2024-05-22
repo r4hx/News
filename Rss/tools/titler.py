f5b314c0b9ffc09c28fb5318fa3ddb6efedb8061
Egorov Egor <comfier.impacts_0y@icloud.com>
2024-05-23 01:17:41 +0300

    Обработка ошибки

--- Rss/tools/titler.py
+++ Rss/tools/titler.py
@@ -17,7 +17,15 @@ def get_title_from_source_url(url: str) -> str:
         html_content = response.text
         soup = BeautifulSoup(html_content, "html.parser")
         title_tag = soup.find("title")
-        title = title_tag.text
+        try:
+            title = title_tag.text
+        except AttributeError:
+            logger.exception(
+                f"Не удалось получить название статьи из {url=} с кодом {response.status_code}"
+            )
+            raise Exception(
+                f"Не удалось получить название статьи из {url=} с кодом {response.status_code}"
+            )
         return title
     else:
         logger.exception(
